c367cb8c362e20450791ab94f9b760091ea4871c
Oleksandr Glotov <ag@wwind.ua>
2025-04-15 20:20:08 +0300

    add simple index.html

--- src/components/ui/card.jsx
+++
@@ -1,8 +0,0 @@
-
-export function Card({ children }) {
-  return <div className="border rounded-2xl shadow-md bg-white dark:bg-gray-900 p-4">{children}</div>
-}
-
-export function CardContent({ children, className }) {
-  return <div className={className}>{children}</div>
-}
--- src/components/ui/progress.jsx
+++
@@ -1,8 +0,0 @@
-
-export function Progress({ value }) {
-  return (
-    <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-      <div className="bg-blue-600 h-2.5 rounded-full" style={{ width: value + '%' }}></div>
-    </div>
-  );
-}
